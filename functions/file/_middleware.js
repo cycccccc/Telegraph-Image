00e7352f62a7184ebbb83eb0809ff6ce0b638fa2
cheny <cheny@DESKTOP-0RB1VN8>
2022-11-03 20:50:40 +0800

    Update _middleware.js

--- functions/file/_middleware.js
+++ functions/file/_middleware.js
@@ -10,6 +10,7 @@ const errorHandler = async ({ next }) => {
 const cors = async ({ next }) => {
     const response = await next(); 
     response.headers.set('Access-Control-Allow-Origin', '*'); 
+    response.headers.set('X-Hello', 'Hello from functions Middleware!');
     return response;
 };
 
